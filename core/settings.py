162e7c349894f41f4a30836d195e0a209862a5a9
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-10-03 16:18:13 -0300

    Inserção do APP no settings.py

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-     "rest_framework",
+    "rest_framework",
+    "FabricaClass",
+    
 ]
 
 MIDDLEWARE = [
