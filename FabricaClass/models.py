57e283e437c0de2fd4c4283200aa2d34c8fe2df8
ldmfabio <ldmfabio@gmail.com>
2023-10-31 11:34:23 -0300

    Criação subclasse Turno em classe Curso

--- FabricaClass/models.py
+++ FabricaClass/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Curso(models.Model):
+    class TurnoCurso(models.IntegerChoices):
+        Matutino = (1, "Matutino",)
+        Vespertino = (2, "Vespertino",)
+        Noturno = (3, "Noturno",)
+        Integral = (4, "Integral",)
     nome = models.CharField(max_length=255, blank=True, null=True)
-    turno = models.CharField(max_length=10, blank=True, null=True)
+    turno = models.IntegerField(choices=TurnoCurso.choices,  default=TurnoCurso.Integral)
 class Turma(models.Model):
     nome = models.CharField(max_length=10, blank=True, null=True)
     curso = models.ForeignKey(Curso, on_delete=models.PROTECT, related_name='curso')
+
