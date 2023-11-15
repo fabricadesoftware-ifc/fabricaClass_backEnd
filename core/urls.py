986dbf736ac34369e43799007b9e57f72e9f20b7
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-15 05:23:21 -0800

    fix in core/urls.py

--- core/urls.py
+++ core/urls.py
@@ -7,16 +7,5 @@ import requests
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(usuario_router.urls)),
-    path('send-email/', SendEmailView.as_view(), name='send-email'),
+    path('send-email/', SendEmailView.as_view(), name='email'),
 ]
-
-
-data = {
-    'subject': 'Assunto e-mail',
-    'message': 'Corpo do e-mail',
-    'recipient_email': 'lucasantonete@gmail.com'
-
-}
-
-response = requests.post('http://link-aplicação/send-email/', data=data)
-print(response.status_code, response.content)
