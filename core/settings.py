8828c6919ecb5913c08c28213a92ec6cdb2c3bab
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-15 07:22:16 -0800

    (#6) fix in settings.py to send emails with gmail port 587

--- core/settings.py
+++ core/settings.py
@@ -119,8 +119,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "usuario.Usuario"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.live.com'
-EMAIL_PORT = 25
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'lucasantonete@hotmail.com'
+EMAIL_HOST_USER = 'suporte.fabricaclass@gmail.com'
 EMAIL_HOST_PASSWORD = ''
