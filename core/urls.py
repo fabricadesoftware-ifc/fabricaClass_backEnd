4e6d841f59b4b380501242d7b15b7c27a6c45650
LucasGabrielAntonete <lucasantonete@gmail.com>
2023-11-15 05:26:48 -0800

    fix in core/urls-2

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
 from django.urls import include,path
 from usuario.router import router as usuario_router
-from FabricaClass.views import SendEmailView
-import requests
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(usuario_router.urls)),
-    path('send-email/', SendEmailView.as_view(), name='email'),
+    path('api/', include('envioEmail.urls'))
 ]
